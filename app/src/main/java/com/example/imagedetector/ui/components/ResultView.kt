3e5a290e1d32a60ce05b064ba90553fe14027b60
shreesha-21 <imsmb21@gmail.com>
2026-01-22 01:53:24 +0530

    1) Made some changes to the imports 2) The composable now uses the new utility function to fetch the bitmap 3) Added a log to log the response from the server 4) Made some changes to the ui layout 5) Added the reset button to this composable

--- app/src/main/java/com/example/imagedetector/ui/components/ResultView.kt
+++ app/src/main/java/com/example/imagedetector/ui/components/ResultView.kt
@@ -1,14 +1,19 @@
 package com.example.imagedetector.ui.components
 
 import android.graphics.BitmapFactory.*
+import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -16,7 +21,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import com.example.imagedetector.R
 import com.example.imagedetector.data.DetectionResponse
+import com.example.imagedetector.decodeAndRotateBitmap
 import java.io.File
 
 //  Composable which shows the result from the server by overlaying a box on the captured image
@@ -27,9 +35,23 @@ fun ResultView(
     onReset: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val bitmap = remember(file) { decodeFile(file.absolutePath) }
+    val bitmap = remember(file) { decodeAndRotateBitmap(file) }
 
-    Column(modifier = modifier.fillMaxSize()) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+
+        //  Log the number of responses received from the server
+        if (detectionResponse.responseSize() != 0) {
+            Log.d("RESPONSE_DEBUG", "Objects detected: ${detectionResponse.responseSize()}")
+        }
+        else {
+            Log.d("RESPONSE_DEBUG", "No objects detected")
+        }
+
+        //   Draws the captured image along with objects detected
         Box(modifier = Modifier.weight(1f).fillMaxWidth()) {
             Image(
                 bitmap = bitmap.asImageBitmap(),
@@ -37,32 +59,37 @@ fun ResultView(
                 modifier = Modifier.fillMaxSize(),
                 contentScale = ContentScale.FillWidth
             )
-        }
-        Canvas(modifier = Modifier.fillMaxSize()) {
-            val scaleX = size.width / bitmap.width
-            val scaleY = scaleX
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                val scaleX = size.width / bitmap.width
+                val scaleY = scaleX
 
-            detectionResponse.detections.forEach { obj ->
-                // Extract coordinates from the list
-                // The server sends [x1, y1, x2, y2]
-                val x1 = obj.box[0]
-                val y1 = obj.box[1]
-                val x2 = obj.box[2]
-                val y2 = obj.box[3]
+                detectionResponse.detections.forEach { obj ->
+                    // Extract coordinates from the list
+                    // The server sends [x1, y1, x2, y2]
+                    val x1 = obj.box[0]
+                    val y1 = obj.box[1]
+                    val x2 = obj.box[2]
+                    val y2 = obj.box[3]
 
-                // Apply scaling (Screen Size / Image Size)
-                val left = x1 * scaleX
-                val top = y1 * scaleY
-                val right = x2 * scaleX
-                val bottom = y2 * scaleY
+                    // Apply scaling (Screen Size / Image Size)
+                    val left = x1 * scaleX
+                    val top = y1 * scaleY
+                    val right = x2 * scaleX
+                    val bottom = y2 * scaleY
 
-                drawRect(
-                    color = Color.Red,
-                    topLeft = Offset(left, top),
-                    size = Size(right - left, bottom - top),
-                    style = Stroke(width = 8f)
-                )
+                    drawRect(
+                        color = Color.Red,
+                        topLeft = Offset(left, top),
+                        size = Size(right - left, bottom - top),
+                        style = Stroke(width = 8f)
+                    )
+                }
             }
+        }
+
+        //  Button to click a new Image
+        Button(onReset) {
+            Text(stringResource(R.string.retry_button_text))
         }
     }
 }
