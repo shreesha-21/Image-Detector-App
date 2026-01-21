dc48178ff4507b3406c57ab81eb72c0eefdf477c
shreesha-21 <imsmb21@gmail.com>
2026-01-22 01:43:30 +0530

    Added a new method to the DetectionResponse data class

--- app/src/main/java/com/example/imagedetector/data/dto.kt
+++ app/src/main/java/com/example/imagedetector/data/dto.kt
@@ -4,7 +4,11 @@ import com.google.gson.annotations.SerializedName
 
 data class DetectionResponse(
     val detections: List<DetectionObject>
-)
+) {
+    fun responseSize(): Int {
+        return detections.size
+    }
+}
 
 //  Data class to be used as blueprint to convert json response from the server into kotlin object
 data class DetectionObject(
@@ -12,4 +16,3 @@ data class DetectionObject(
     val confidence: Float,
     @SerializedName("bbox") val box: List<Float>
 )
-
